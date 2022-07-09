30e2f501dfdf628c447a7f491806d3a46091e516
oange6214 <oange6214@gmail.com>
2022-07-09 12:30:13 +0800

    WPF MVVM TUTORIAL #3

---
+++ WPF_MVVM_Reserve/ViewModels/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_MVVM_Reserve.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public ViewModelBase CurrentViewModel { get; }
+        public MainViewModel()
+        {
+            //CurrentViewModel = new MakeReservationViewModel();
+            CurrentViewModel = new ReservationListingViewModel();
+        }
+    }
+}
